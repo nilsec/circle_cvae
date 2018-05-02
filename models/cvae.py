21a24c14c8b9113d1201b562f170d0c556b5ad83
Nils Eckstein <ecksteinn@c04u01.int.janelia.org>
2018-05-02 10:20:04 -0400

    Finalize train pipeline

--- models/cvae.py
+++ models/cvae.py
@@ -1,12 +1,35 @@
+import sys
+sys.path.insert(0, './..')
+
+
 from layers_mmd import encoder, decoder, sample_z, kl
+import os
+from data import Circle
 import numpy as np
 import tensorflow as tf
+import h5py
 
 
-def train(in_height, 
-          in_width,
+def train(size, 
+          radius,
           batch_size,
+          beta,
+          epochs,
+          snapshot_dir,
+          checkpoint_dir,
           z_dim=20):
+
+    if not os.path.exists(snapshot_dir):
+        os.makedirs(snapshot_dir)
+
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+
+    in_height = size
+    in_width = size
+
+    circle = Circle(size, 
+                    radius)
 
     x = tf.placeholder(tf.float32, shape=[batch_size, 
                                           in_height, 
@@ -17,24 +40,80 @@ def train(in_height,
                                           in_height, 
                                           in_width, 
                                           1])
+    print("XY ENCODER:")
     xy = tf.concat([x,y], axis=-1)
+    print("xy: " + str(xy.get_shape().as_list()))
 
     latents = encoder(xy, 2*z_dim, scope="xy_encoder")
+    print("latents: " + str(latents.get_shape().as_list()))
 
     mean = latents[:,:z_dim]
     log_sigma = latents[:,z_dim:]
-
     z = sample_z(mean, log_sigma)
-    x = encoder(x, z_dim, scope="x_encoder")
 
-    xz = tf.concat([x,z], axis=-1)
+    print("X ENCODER:")
+    print("x: " + str(x.get_shape().as_list()))
+    x_enc = encoder(x, z_dim, scope="x_encoder")
+
+    print("x_enc: " + str(x.get_shape().as_list()))
+    print("z: " + str(z.get_shape().as_list()))
+    xz = tf.concat([x_enc,z], axis=-1)
+    print("xz: " + str(xz.get_shape().as_list()))
 
     y_logits = decoder(xz)
+    print("y_logits: " + str(y_logits.get_shape().as_list()))
     y_out = tf.sigmoid(y_logits)
 
+    kl_loss = kl(mean, log_sigma, batch_size)
+    ce = tf.losses.sigmoid_cross_entropy(y,
+                                         y_logits)
+    
+    loss = beta * kl_loss + ce
+    opt = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(loss)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+    
+    for i in range(epochs):
+        batch_x, batch_y = circle.next_batch(batch_size)
+        batch_x = batch_x.reshape(batch_size, in_height, in_width, 1)
+        batch_y = batch_y.reshape(batch_size, in_height, in_width, 1)
+
+        _, ce_i, kl_i, z_i, y_out_i =\
+                sess.run([opt, ce, kl_loss, z, y_out], feed_dict={x: batch_x,
+                                                                  y: batch_y})
+        if i % 100 == 0:
+            print("iteration {} - ce: {}, kl: {}".format(i, ce_i, kl_i))
+
+        if i % 1000 == 0:
+            snapshot_path = os.path.join(snapshot_dir, "snap_{}.h5".format(i))
+            with h5py.File(snapshot_path, 'w') as f:
+                f.create_dataset(name="x",
+                                 data=batch_x[:,:,:,0],
+                                 compression='gzip')
+
+                f.create_dataset(name="y",
+                                 data=batch_y[:,:,:,0],
+                                 compression='gzip')
+
+                f.create_dataset(name="y_out",
+                                 data=y_out_i[:,:,:,0],
+                                 compression='gzip')
+
+                f.create_dataset(name="z",
+                                 data=z_i,
+                                 compression='gzip')
+
+    sess.close()
+
 if __name__ == "__main__":
-    train(28, 
-          28,
-          20,
-          20)
+    train(size=28, 
+          radius=12,
+          batch_size=200,
+          beta=1.0,
+          epochs=100000,
+          snapshot_dir="./snapshots/run_0",
+          checkpoint_dir="./checkpoints/run_0",
+          z_dim=2)
+
 
--- models/layers_mmd.py
+++ models/layers_mmd.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
 import numpy as np
 
-def conv2d_relu(inputs, num_outputs, kernel_size, stride):
-    conv = tf.contrib.layers.convolution2d(inputs, num_outputs, kernel_size, stride,
-                                           weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                           activation_fn=tf.identity)
-    conv = tf.nn.relu(conv)
+def lrelu(x, rate=0.1):
+    return tf.maximum(tf.minimum(x * rate, 0), x)
+
+def conv2d_lrelu(inputs, num_outputs, kernel_size, stride):
+    conv = tf.contrib.layers.convolution2d(
+                                        inputs, 
+                                        num_outputs, 
+                                        kernel_size, 
+                                        stride,
+                                        weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                        activation_fn=tf.identity)
+    conv = lrelu(conv)
     return conv
 
 def conv2d_t_relu(inputs, num_outputs, kernel_size, stride):
@@ -15,10 +22,12 @@ def conv2d_t_relu(inputs, num_outputs, kernel_size, stride):
     conv = tf.nn.relu(conv)
     return conv
 
-def fc_id(inputs, num_outputs):
+def fc_lrelu(inputs, num_outputs):
     fc = tf.contrib.layers.fully_connected(inputs, num_outputs,
                                            weights_initializer=tf.contrib.layers.xavier_initializer(),
                                            activation_fn=tf.identity)
+
+    fc = lrelu(fc)
     return fc
 
 def fc_relu(inputs, num_outputs):
@@ -28,13 +37,15 @@ def fc_relu(inputs, num_outputs):
     fc = tf.nn.relu(fc)
     return fc
 
+def fc_id(inputs, num_outputs):
+    fc = tf.contrib.layers.fully_connected(inputs, num_outputs,
+                                           weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                           activation_fn=tf.identity)
+
+    return fc
 
 def sample_z(mean,
              log_sigma):
-
-    print("SampleZ:")
-    print(" mean: " + str(mean.get_shape().as_list()))
-    print(" log_sigma: " + str(log_sigma.get_shape().as_list()))
 
     dim = mean.get_shape().as_list()
     normal = tf.contrib.distributions.MultivariateNormalDiag(tf.zeros(dim), tf.ones(dim))
@@ -43,37 +54,43 @@ def sample_z(mean,
     z_sigma = tf.multiply(tf.exp(log_sigma), sample)
     z = tf.add(mean, z_sigma)
 
-    print(" z: " + str(z.get_shape().as_list()))
     return z[0,:,:]
 
 
 def kl(mean, log_sigma, batch_size, free_bits=0.0):
-    print("KL:")
-    print(" mean: " + str(mean.get_shape().as_list()))
-    print(" log_sigma: " + str(log_sigma.get_shape().as_list()))
-
     kl_div = tf.reduce_sum(tf.maximum(free_bits,
                                       0.5 * (tf.square(mean) + tf.exp(2 * log_sigma) - 2 * log_sigma - 1)))
     kl_div /= float(batch_size)
-    print(" kl_div: " + str(kl_div.get_shape().as_list()))
     return kl_div
 
 
 # Encoder and decoder use the DC-GAN architecture
 def encoder(x, z_dim, scope="encoder"):
+    print("ENCODER:")
     with tf.variable_scope(scope):
-        conv1 = conv2d_relu(x, 64, 4, 2)
-        conv2 = conv2d_relu(conv1, 128, 4, 2)
+        conv1 = conv2d_lrelu(x, 64, 4, 2)
+        print(" conv1: " + str(conv1.get_shape().as_list()))
+        conv2 = conv2d_lrelu(conv1, 128, 4, 2)
+        print(" conv2: " + str(conv2.get_shape().as_list()))
         conv2 = tf.reshape(conv2, [-1, np.prod(conv2.get_shape().as_list()[1:])])
-        fc1 = fc_id(conv2, 1024)
+        print(" reshape: " + str(conv2.get_shape().as_list()))
+        fc1 = fc_lrelu(conv2, 1024)
+        print(" fc1: " + str(fc1.get_shape().as_list()))
         fc2 = fc_id(fc1, z_dim)
+        print(" fc2: " + str(fc2.get_shape().as_list()))
         return fc2
 
 def decoder(z):
+    print("DECODER")
     with tf.variable_scope('decoder'):
         fc1 = fc_relu(z, 1024)
+        print(" fc1: " + str(fc1.get_shape().as_list()))
         fc2 = fc_relu(fc1, 7*7*128)
+        print(" fc2: " + str(fc2.get_shape().as_list()))
         fc2 = tf.reshape(fc2, tf.stack([tf.shape(fc2)[0], 7, 7, 128]))
+        print(" reshape: " + str(fc2.get_shape().as_list()))
         conv1 = conv2d_t_relu(fc2, 64, 4, 2)
+        print(" conv1t: " + str(conv1.get_shape().as_list()))
         output = tf.contrib.layers.convolution2d_transpose(conv1, 1, 4, 2, activation_fn=tf.identity)
+        print(" conv2t: " + str(output.get_shape().as_list())) 
         return output
