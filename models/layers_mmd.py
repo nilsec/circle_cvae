ab83d5ea66160d842c0a571075dd2b420a6d3446
Nils Eckstein <ecksteinn@c04u01.int.janelia.org>
2018-05-02 11:19:47 -0400

    Add mmd loss

--- models/layers_mmd.py
+++ models/layers_mmd.py
@@ -64,6 +64,22 @@ def kl(mean, log_sigma, batch_size, free_bits=0.0):
     return kl_div
 
 
+def compute_kernel(x, y):
+    x_size = tf.shape(x)[0]
+    y_size = tf.shape(y)[0]
+    dim = tf.shape(x)[1]
+    tiled_x = tf.tile(tf.reshape(x, tf.stack([x_size, 1, dim])), tf.stack([1, y_size, 1]))
+    tiled_y = tf.tile(tf.reshape(y, tf.stack([1, y_size, dim])), tf.stack([x_size, 1, 1]))
+    return tf.exp(-tf.reduce_mean(tf.square(tiled_x - tiled_y), axis=2) / tf.cast(dim, tf.float32))
+
+
+def compute_mmd(x, y):
+    x_kernel = compute_kernel(x, x)
+    y_kernel = compute_kernel(y, y)
+    xy_kernel = compute_kernel(x, y)
+    return tf.reduce_mean(x_kernel) + tf.reduce_mean(y_kernel) - 2 * tf.reduce_mean(xy_kernel)
+
+
 # Encoder and decoder use the DC-GAN architecture
 def encoder(x, z_dim, scope="encoder"):
     print("ENCODER:")
@@ -79,6 +95,7 @@ def encoder(x, z_dim, scope="encoder"):
         fc2 = fc_id(fc1, z_dim)
         print(" fc2: " + str(fc2.get_shape().as_list()))
         return fc2
+
 
 def decoder(z):
     print("DECODER")
