877c3f52ec0f2334638c2774c1f1b15d56277068
Nils <nils.eckstein@googlemail.com>
2018-05-02 14:11:37 +0200

    Fix gt generation and add plotting

--- data/circles.py
+++ data/circles.py
@@ -1,39 +1,48 @@
 import numpy as np
 from numpy.random import randint
+import matplotlib.pyplot as plt
+
 
 class Circle(object):
     def __init__(self, 
-                 width, 
-                 height, 
+                 size, 
                  radius):
-        assert(width % 2 == 0)
-        assert(height % 2 == 0)
+        
+        assert(size % 2 == 0)
         assert(radius % 2 == 0)
 
-        self.width = width
-        self.height = height
+        self.size = size
         self.radius = radius
         self.x = self.gen_x()
 
     def gen_x(self):
-        x = np.zeros([self.height, self.width])
-        xx,yy = np.mgrid[:self.height, :self.width]
-        circle = (xx - self.height/2)**2 + (yy - self.width)**2 <= self.radius**2
+        x = np.zeros([self.size, self.size])
+        xx,yy = np.mgrid[:self.size, :self.size]
+        circle = (xx - self.size/2)**2 + (yy - self.size/2)**2 <= self.radius**2
         return np.array(np.logical_or(x, circle), dtype=np.float32)
     
     def next_batch(self, 
-                   batch_size,
-                   ):
+                   batch_size):
+
         x = np.stack([self.x] * batch_size, axis=0)
-        print(np.shape(x))
-        y = np.zeros([batch_size, self.width, self.height])
-        gt = randint(low=-self.radius/2,
-                        high=self.radius/2 + 1,
-                        size=(batch_size, 1,1))
-        #y[gt] = 1.0
-        return x
+        y = np.zeros([batch_size, self.size, self.size])
+
+        gt = randint(low=(self.size-self.radius)/2,
+                     high=(self.size + self.radius)/2 + 1,
+                     size=(batch_size, 2))
+
+        gt = np.insert(gt, 0, np.arange(batch_size), axis=1)
+
+        y[tuple(np.array(gt).T)] = 1.0
+        return x, y
+
+    def plot_batch(self, batch, batch_number):
+        plt.imshow(batch[0][batch_number,:,:])
+        plt.imshow(batch[1][batch_number,:,:], alpha=0.5)
+        plt.show()
 
 
 if __name__== "__main__":
-    c = Circle(28, 28, 12)
-    print(c.next_batch(2))
+    c = Circle(28, 12)
+    for j in range(200):
+        batch = c.next_batch(200)
